98f15bc6ccb90819fb22ec86d8fdc38475cc528b
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-03-29 13:45:26 +1100

    Use Bogus to seed data (#14)

--- src/ContosoUniversity.API/Startup.cs
+++ src/ContosoUniversity.API/Startup.cs
@@ -1,22 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.API.Data;
-using NJsonSchema;
-using NSwag.AspNetCore;
-using System.Reflection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Azure;
-using Azure.Core.Extensions;
 
 namespace ContosoUniversity.API
 {
